0e128fa583a6e834e4a5285f71d44b6786ec53de
Chao Xie <chaoranxie@gmail.com>
2017-06-17 11:59:08 -0400

    create course and course list components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyCs0ZcISP7-jn_msQvtv3qixoUd41qGcRM",
+authDomain: "semicolon-ptc.firebaseapp.com",
+databaseURL: "https://semicolon-ptc.firebaseio.com",
+projectId: "semicolon-ptc",
+storageBucket: "semicolon-ptc.appspot.com",
+messagingSenderId: "467324580481"
+}
 };
